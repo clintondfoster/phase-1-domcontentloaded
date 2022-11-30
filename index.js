f458b5a4493caecb78c5bb88c9219c80f8c6c7fc
clinton foster <clintondavidfoster@gmail.com>
2022-11-30 07:39:21 -0500

    commit tree 4486c283b73dec8cd3860c49901e38fa1704b1e3

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    newDomLoad;
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );// Your code goes here
+  
+  function newDomLoad() {
+    document.getElementById("#text").textContent = "This is really cool!"
+}
